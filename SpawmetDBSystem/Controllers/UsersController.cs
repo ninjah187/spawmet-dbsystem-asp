37bc3235d7d67359bf68077c6f634669c1738ec5
ninjah187 <ninjah187@gmail.com>
2015-01-27 04:14:08 +0100

    more tables features: selecting and buttons

--- SpawmetDBSystem/Controllers/UsersController.cs
+++ SpawmetDBSystem/Controllers/UsersController.cs
@@ -50,5 +50,77 @@ namespace SpawmetDBSystem.Controllers
                 return RedirectToAction("Login");
             }
         }
+
+        public ActionResult Create()
+        {
+            var userGroups = dbContext.UserGroups.ToList();
+            return View(userGroups);
+        }
+
+        [HttpPost]
+        public ActionResult Create(string login, string password, string groupName)
+        {
+            /*var user = dbContext.Users.Single(u => u.Login == login);
+            if (user == null)
+            {
+                var group = dbContext.UserGroups.Single(ug => ug.Name == groupName);
+                user = new User()
+                {
+                    Login = login,
+                    Password = Tools.GetMd5Hash(password),
+                    UserGroup = group,
+                };
+                dbContext.Users.Add(user);
+                dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                throw new UserAlreadyExistsException();
+            }*/
+            User user;
+            try
+            {
+                user = dbContext.Users.Single(u => u.Login == login);
+            }
+            catch (InvalidOperationException exception) //occurs when there's no such user in database
+            {
+                var group = dbContext.UserGroups.Single(ug => ug.Name == groupName);
+                user = new User()
+                {
+                    Login = login,
+                    Password = Tools.GetMd5Hash(password),
+                    UserGroup = group,
+                };
+                dbContext.Users.Add(user);
+                dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            //if there's user with such login
+            throw new UserAlreadyExistsException();
+        }
+
+        public ActionResult Delete(string deleteString)
+        {
+            string[] deleteIds = deleteString.Split(',');
+            var deleteUsers = new List<User>();
+            for (int i = 0; i < deleteIds.Length; i++)
+            {
+                var deleteUser = dbContext.Users.Single(u => u.ID == Int32.Parse(deleteIds[i]));
+                deleteUsers.Add(deleteUser);
+            }
+            return View(deleteUsers);
+        }
+
+    }
+
+    public class UserAlreadyExistsException : Exception
+    {
+        public UserAlreadyExistsException()
+            : base("Taki użytkownik już istnieje.")
+        {
+        }
     }
 }
